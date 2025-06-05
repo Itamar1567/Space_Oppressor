50490d1dbdebfc8c707fa829b0c009afb9c906b8
Itamar1567 <Itamarbaharav@gmail.com>
2025-06-04 20:55:16 -0400

    Added a win scenario, created a border for the enemies to hit when they miss the player, added player health, so that when the player is hit, they lose it and the game is over, added a background image

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ import random
 
 pygame.init()
 
+class Border:
+    def __init__(self, width, posY):
+        self.width = width
+        self.posY = posY
+        self.height = 50
+        self.color = (128,0,128)
+        self.posYOffset = 15
+        self.rect = pygame.Rect(0,self.posY -self.posYOffset, width, self.height)
+    def DebugDraw(self, surface):
+        pygame.draw.rect(surface,self.color,self.rect)
+    def GetRect(self):
+        return self.rect
+        
+        
+        
+        
 class Bullet:
     def __init__(self, speed,startPosX, startPosY,color, screen):
         self.surface = screen
@@ -18,6 +34,7 @@ class Bullet:
         self.rect.y -= self.speed
     def GetRect(self):
         return self.rect
+
 class Enemy:
     
     def __init__(self, screenX, screenY, radius, color, enemySpeed):
@@ -26,7 +43,7 @@ class Enemy:
         self.radius = radius
         self.color = color
         self.posX = random.randint(0, screenX)
-        self.posY = 0 -random.randint(50, 1000)
+        self.posY = 0 -random.randint(50, 9000)
         self.width = 50
         self.height = 50
         self.enemyRect = None
@@ -34,7 +51,7 @@ class Enemy:
         
     def Spawn(self, surface):
         self.enemyRect = pygame.Rect(self.posX,self.posY, self.width, self.height)
-        pygame.draw.rect(screen, self.color, self.enemyRect) 
+        pygame.draw.rect(surface, self.color, self.enemyRect) 
     def Move(self):
         self.posY += self.speed 
     def GetEnemy(self):
@@ -46,6 +63,7 @@ class Enemy:
 class Player:
     
     def __init__(self, x, y, radius, color):
+        self.health = 15
         self.score = 0
         self.x = x
         self.y = y
@@ -59,7 +77,10 @@ class Player:
 
         self.playerRect = pygame.Rect(posX-50,posY-50, self.width, self.height)
         pygame.draw.rect(surface, self.color, self.playerRect)
-        
+    
+    def GetHealth(self):
+        return self.health    
+    
     def GetPlayer(self):
         return self.playerRect
     
@@ -67,20 +88,27 @@ class Player:
         return self.score 
     def SetScore(self, incrementer):
         self.score += incrementer
+    def Die(self):
+        pygame.quit()
+    def DecreaseHealth(self, damage):
+        self.health -= damage
+            
 #Attributes
-enemySpeed = 5
+enemySpeed = 3
 bulletSpeed = 15
 
 #UI
+backgroundImage = pygame.image.load("Space.jpg")
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 font = pygame.font.SysFont('Arial', 30)
+winFont = pygame.font.SysFont('Arial', 50)
 screenX = 1920
 screenY = 1080
 screen = pygame.display.set_mode((screenX,screenY))
 pygame.display.set_caption("Spaceship Game")
-center_x = screenX//2
+center_X = screenX//2
 center_Y = screenY//2
 radius = 75
 border = 2
@@ -88,10 +116,10 @@ border = 2
 clock = pygame.time.Clock()
 
 #Object Instantiation
-player = Player(center_x,center_Y, radius, white)
+player = Player(center_X,center_Y, radius, white)
 enemies = []
-for _ in range(15):
-    new_enemy = Enemy(screenX , screenY, radius, red,enemySpeed)
+for _ in range(1):
+    new_enemy = Enemy(screenX , screenY, radius, red, enemySpeed)
     enemies.append(new_enemy)
 
 bullets = []
@@ -99,6 +127,7 @@ bullets = []
 enemiesToRemove = []
 bulletsToRemove = []
 
+border = Border(screenX,screenY)
 running = True
 while running:
     for event in pygame.event.get():
@@ -108,15 +137,29 @@ while running:
                 bullets.append(new_bullet)
         if event.type == pygame.QUIT:
             running = False
+    if player.GetHealth() <= 0:
+        print("entered")
+        player.Die()
     mouseX, mouseY = pygame.mouse.get_pos()
     playerShootPoint = mouseY - 75
-    screen.fill(black)
+    screen.blit(backgroundImage,(0,0))
+    #screen.fill(black)
+    border.DebugDraw(screen)
     for bullet in bullets:
         bullet.Shoot()
+        if bullet.GetRect().y < 0:
+            print("Bullet out of bounds")
+            bulletsToRemove.append(bullet)
     for enemy in enemies:
         enemy.Spawn(screen)
         enemy.Move()
         enemyRect = enemy.GetEnemy()
+        if enemyRect and player.GetPlayer().colliderect(enemyRect):
+            enemiesToRemove.append(enemy)
+            player.DecreaseHealth(1)
+        if enemyRect and border.GetRect().colliderect(enemyRect):
+            enemiesToRemove.append(enemy)
+            player.DecreaseHealth(1) 
         for new_bullet in bullets:
             if enemyRect and new_bullet.GetRect().colliderect(enemyRect):
                 #Adds objects that need to be removed into their corresponding removal array
@@ -139,10 +182,18 @@ while running:
     bulletsToRemove.clear()
     #Function to move the player based on mouse position
     player.Move(screen,mouseX,mouseY)
-    #Renders a text with the player's score
-    scoreText = font.render("Score: " + str(player.GetScore()), False, white)
-    #Adds the score text to the screen
+    #Renders a text with the player's score and one with the health
+    scoreText = font.render("Score: " + str(player.GetScore()), True, white)
+    healthText = font.render("Health: " + str(player.GetHealth()), True, white)
+    winText = winFont.render("Congratulations you win!!!", True, white)
+    #Adds the texts to the screen
     screen.blit(scoreText, (100,100))
+    screen.blit(healthText, (100,150))
+    print(len(enemies))
+    if len(enemies) <= 0:
+        print(len(enemies))
+        screen.blit(winText,(center_X,center_Y))
+    #Background Image
     pygame.display.update()
     clock.tick(60)
 
